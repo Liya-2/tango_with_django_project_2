3cb7fedf6afea239592a03b0ec18a1cc83df2898
Liang He <2538931h@student.gla.ac.uk>
2021-07-26 17:12:59 +0100

    add templates and index.html

--- rango/urls.py
+++ rango/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from rango import views
 
 urlpatterns = [
+    path('about/', views.about, name='about'),
     path('', views.index, name='index'),
-
 ]
--- rango/views.py
+++ rango/views.py
@@ -6,5 +6,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    # return HttpResponse("Rango says hey there partner!")
 
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
