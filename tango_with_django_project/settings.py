8fa38b6c51b5f6238bf757dc4edd7a4379417868
Liang He <2538931h@student.gla.ac.uk>
2021-07-26 17:35:16 +0100

    ch4_v2: add static and rango.jpg

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -70,6 +70,11 @@ TEMPLATES = [
     },
 ]
 
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 
