f0a88511d314bbd51f2c5af0077d662014d2eb07
Liang He <2538931h@student.gla.ac.uk>
2021-07-26 16:19:04 +0100

    ch3_v1

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
+
     path('admin/', admin.site.urls),
 ]
