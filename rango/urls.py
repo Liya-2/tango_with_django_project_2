ab812bd83c69e362181156bdc5b7b3e788abd34d
Liang He <2538931h@student.gla.ac.uk>
2021-07-27 09:44:16 +0100

    ch3_v2: test OK

--- rango/urls.py
+++ rango/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from rango import views
 
+app_name = 'rango'
+
 urlpatterns = [
+    path('about/', views.about, name='about'),
     path('', views.index, name='index'),
 
 ]
